1386674f5c5833b6d886617771a472bf1c8ee671
madrabit <madrabot@gmail.com>
2020-09-15 19:17:47 +0300

    0. Видео[#276753]

---
+++ src/main/java/ru/job4j/accident/WebInit.java
@@ -0,0 +1,27 @@
+package ru.job4j.accident;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import ru.job4j.accident.config.WebConfig;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author madrabit on 15.09.2020
+ * @version 1$
+ * @since 0.1
+ */
+public class WebInit implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletCxt) {
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(WebConfig.class);
+        ac.refresh();
+        DispatcherServlet servlet = new DispatcherServlet(ac);
+        ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
---
+++ src/main/java/ru/job4j/accident/config/WebConfig.java
@@ -0,0 +1,27 @@
+package ru.job4j.accident.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author madrabit on 15.09.2020
+ * @version 1$
+ * @since 0.1
+ */
+@Configuration
+@ComponentScan("ru.job4j.accident")
+public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("./WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
